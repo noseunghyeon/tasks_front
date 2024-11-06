c20d41922f10700b2c9b93c1bff03aba0dcedd35
noseunghyeon <ro981009@gmail.com>
2024-11-06 16:32:25 +0900

    first commit

---
+++ src/Proceeding/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navibar from "../components/Navibar";
+
+const index = () => {
+  return (
+    <div className="page-section">
+      <Navibar />
+    </div>
+  );
+};
+
+export default index;
