be7563bfb3601099603e9b67d36dc581130f7587
noseunghyeon <ro981009@gmail.com>
2024-11-07 17:42:35 +0900

    new

--- src/components/Itempanel.jsx
+++ src/components/Itempanel.jsx
@@ -1,12 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import PageTitle from "./PageTitle";
 import Item from "./Item";
 import AddItem from "./AddItem";
+import { fetchGetItemsData } from "../redux/slices/apiSlice";
 
 const Itempanel = ({ pageTitle }) => {
   const authData = useSelector((state) => state.auth.authData);
   const userKey = authData?.sub;
+  const dispatch = useDispatch();
+
+  // console.log(userKey);
+
+  const getTasksData = useSelector((state) => state.apis.getItemsData);
+  console.log(getTasksData);
+
+  useEffect(() => {
+    if (!userKey) return;
+
+    const fetchGetItems = async () => {
+      try {
+        await dispatch(fetchGetItemsData(userKey)).unwrap(); //useEffect 내부에서 dispatch 함수를 호출하여 thunk 함수 실행
+      } catch (error) {
+        console.error("Failed to Fetch Items: ", error);
+      }
+    };
+    fetchGetItems();
+  }, [dispatch, userKey]);
+
   return (
     <div className="panel bg-[#212121] w-4/5 h-full rounded-md border border-gray-500 py-5 px-4 overflow-y-auto">
       {userKey ? (
