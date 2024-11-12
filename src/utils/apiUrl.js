2f439907ec473ae8a24754230bf8aedda3ac1c88
noseunghyeon <ro981009@gmail.com>
2024-11-12 17:19:12 +0900

    11121718

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -4,10 +4,12 @@ const GET_TASKS_API_URL = `${rootPath}/getTasks`;
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`;
 const POST_TASK_API_URL = `${rootPath}/postTask`;
 const UPDATE_TASK_API_URL = `${rootPath}/updateTask`;
+const UPDATE_COMPLETED_TASK_API_URL = `${rootPath}/updateCompletedTask`;
 
 export {
   GET_TASKS_API_URL,
   DELETE_TASK_API_URL,
   POST_TASK_API_URL,
   UPDATE_TASK_API_URL,
+  UPDATE_COMPLETED_TASK_API_URL,
 };
