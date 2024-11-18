99f2587d3f973908eed6c057e946ad74061836a2
noseunghyeon <ro981009@gmail.com>
2024-11-18 15:44:53 +0900

    update

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
 // const rootPath = "http://localhost:8000";
-const rootPath = "http://3.85.134.110";
+const rootPath = "https://back.seunghyeon.site";
 
 const GET_TASKS_API_URL = `${rootPath}/getTasks`;
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`;
